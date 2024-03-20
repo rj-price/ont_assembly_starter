09322672e9e3a35dd5f6a8b4194401b551000021
Jordan Price <rj_price@hotmail.co.uk>
2024-03-20 13:54:38 +0000

    update racon script

--- scripts/racon.sh
+++ scripts/racon.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 #SBATCH -J racon
-#SBATCH --partition=long
-#SBATCH --mem=100G
-#SBATCH --cpus-per-task=16
+#SBATCH --partition=medium
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=8
 
-# iterations = $1
-# reads = $2
-# assembly = $3
+# INPUTS
+Reads=$1
+Assembly=$2
+Iterations=$3
+OutDir=$4
 
-fileshort=$(basename $3 | sed s/".fasta"//g)
-/mnt/shared/scratch/jnprice/apps/raconnn/raconnn $1 $2 $3 > "$fileshort"_racon.fasta
+# CHECK INPUTS
+if [[ -d "$ReadsDir" && -n "$Assembly" && -n "$Iterations" && -n "$OutDir" ]]; then
+    # CREATE OUTPUT FOLDER
+    mkdir -p "$OutDir"
 
+    # OUTPUT PREFIX
+    Prefix=$(basename "$Assembly" .fasta)
+
+    # RUN RACON
+    /mnt/shared/scratch/jnprice/apps/raconnn/raconnn $Iterations $Reads $Assembly > "$OutDir"/"$Prefix"_racon.fasta
+
+else
+    # PRINT ERROR & USAGE MESSAGES
+    echo -e "\nERROR: Expected inputs not found. Please provide a FASTQ file (.fq.gz or .fastq.gz required), a minimum read length (in bp), a minimum quality score (integer) and an output directory. \n"
+    echo -e "Usage: sbatch racon.sh <fastq_file.fastq.gz> <assembly.fasta> <number of iterations> <output_directory> \n"
+    exit 1
+fi
