8334123e513b361c656254f7c061bf2edd82f663
Jordan Price <rj_price@hotmail.co.uk>
2024-07-19 15:25:22 +0100

    change compute requirements

--- scripts/XX_busco.sh
+++ scripts/XX_busco.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH -J busco
 #SBATCH --partition=medium
-#SBATCH --mem=4G
+#SBATCH --mem=8G
 #SBATCH --cpus-per-task=8
 
 # INPUTS
@@ -11,12 +11,8 @@ OutDir=$3
 
 # CHECK INPUTS
 if [[ -f "$Assembly" && -n "$Lineage" && -n "$OutDir" ]]; then
-    # CREATE OUTPUT FOLDER
-    mkdir -p "$OutDir"
-
     # RUN BUSCO
     busco -m genome -c 8 -i "$Assembly" -o "$OutDir" -l "$Lineage"_odb10
-
 else
     # PRINT ERROR & USAGE MESSAGES
     echo -e "\nERROR: Expected inputs not found. Please provide an assembly file (.fasta required), a BUSCO lineage (see https://busco.ezlab.org/list_of_lineages.html) and an output directory. \n"
